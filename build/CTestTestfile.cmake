c90620cc4c1f29e3848e28684a963feb7f8c89a5
deGekata <y.zvonkov@yandex.ru>
2022-04-20 22:46:24 +0300

    f

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/c/Users/Yaric-PC/Documents/Git Projects/lab05_final/lab05_
-# Build directory: /mnt/c/Users/Yaric-PC/Documents/Git Projects/lab05_final/lab05_/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(unit_test "/mnt/c/Users/Yaric-PC/Documents/Git Projects/lab05_final/lab05_/build/tests")
-set_tests_properties(unit_test PROPERTIES  _BACKTRACE_TRIPLES "/mnt/c/Users/Yaric-PC/Documents/Git Projects/lab05_final/lab05_/CMakeLists.txt;30;add_test;/mnt/c/Users/Yaric-PC/Documents/Git Projects/lab05_final/lab05_/CMakeLists.txt;0;")
-subdirs("third-party/gtest")
--- build/third-party/gtest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/c/Users/Yaric-PC/Documents/Git Projects/lab05_final/lab05_/third-party/gtest/googlemock
-# Build directory: /mnt/c/Users/Yaric-PC/Documents/Git Projects/lab05_final/lab05_/build/third-party/gtest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
